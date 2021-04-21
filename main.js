8a0fe484c7019ac2a1b04fa5127ac11ebbf5dc9b
M-Tanveer <m.tanveercs@gmail.com>
2021-04-20 21:02:01 -0700

    Programs About Objects

--- main.js
+++ main.js
@@ -103,3 +103,122 @@ console.log("",res);
 //     return str.split(' ').length;
 //     }
 //  console.log(countWords("   Tutorix is one of the best E-learning   platforms"));
+
+
+// 7.Write a JavaScript program to list the properties of a JavaScript object
+// var student = {
+//     name : "David Rayy",
+//     sclass : "VI",
+//     rollno : 12 };
+//     Sample Output: name,sclass,rollno
+// solution:-
+// var student =  { 
+//     name : "David Rayy", 
+//     sclass : "VI", 
+//     rollno : 12  };
+//   console.log("",student.name);
+//    console.log("",student.sclass);
+//    console.log("",student.rollno);
+        
+//  8.Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
+// Sample object:
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+// solution:-
+// var student =  { 
+//     name : "David Rayy", 
+//     sclass : "VI", 
+//     rollno : 12  };
+//   console.log(student);
+//   delete student.rollno;
+//   console.log(student);
+
+//9.Write a JavaScript program to get the length of a JavaScript object.
+// Sample object :
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+// Solution:-
+
+// Object.objsize = function(Myobj) {
+//     var osize = 0, key;
+//     for (key in Myobj) {
+//         if (Myobj.hasOwnProperty(key)) osize++;
+//     }
+//     return osize;
+// };
+
+// var student = { 
+// name : "David Rayy", 
+// sclass : "VI", 
+// rollno : 12 };
+// var objsize = Object.objsize(student);
+// console.log('Size of the current object : '+objsize);
+
+// 10.Write a JavaScript program to display the reading status (i.e. display book name, author name and reading status) of the following books.  
+// var library = [
+// {     author: 'Bill Gates',
+//       title: 'The Road Ahead',
+//              readingStatus: true
+//             },
+
+//    {author: 'Steve Jobs',
+//    title: 'Walter Isaacson',
+//           readingStatus: true
+
+//    },
+
+//    {    author: 'Suzanne Collins',
+//       title:  'Mockingjay: The Final Book of The Hunger Games',
+//       readingStatus: false
+
+//    }];
+// Solution:-
+
+// var library = [ 
+//     {
+//         title: 'Bill Gates',
+//         author: 'The Road Ahead',
+//         readingStatus: true
+//     },
+//     {
+//         title: 'Steve Jobs',
+//         author: 'Walter Isaacson',
+//         readingStatus: true
+//     },
+//     {
+//         title: 'Mockingjay: The Final Book of The Hunger Games',
+//         author: 'Suzanne Collins',
+//         readingStatus: false
+//     }];
+
+// for (var i = 0; i < library.length; i++) 
+//    {
+//     var book = "'" + library[i].title + "'" + ' by ' + library[i].author + ".";
+//     if (library[i].readingStatus) {
+//       console.log("Already read " + book);
+//     } else
+//     {
+//      console.log("You still need to read " + book);
+//     }
+//    }
+
+//11. Write a JavaScript program to get the volume of a Cylinder with four decimal places using object classes.  
+// Volume of a cylinder : V = πr2h
+
+// function Cylinder(cyl_height, cyl_diameter) {
+//     this.cyl_height = cyl_height;
+//     this.cyl_diameter = cyl_diameter;
+//   }
+  
+//   Cylinder.prototype.Volume = function () {
+//     var radius = this.cyl_diameter / 2;
+//     return this.cyl_height * Math.PI * radius * radius;
+//   };
+  
+//   var cyl = new Cylinder(7, 4);
+//   // Volume of the cylinder with four decimal places.
+//   console.log('volume =', cyl.Volume().toFixed(4));
